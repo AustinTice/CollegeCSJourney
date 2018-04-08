28cd46bc4e2961eeaed07cd14661f1dda877a1d3
Austin Tice <austin.tice4@gmail.com>
2018-04-07 19:01:14 -0500

    Adding Arrays.cpp file

---
+++ First Semester Projects - CS1010/Arrays.cpp
@@ -0,0 +1,53 @@
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+// Prototypes
+int average(int arr[]);
+int highest(int arr[]);
+
+int main()
+{
+	// init fish array
+	int fish[10];
+	int caught;
+	// loo[ through getting caught amt
+	for(int i = 0; i < 10; i++){
+		cout << "How many fish did you catch?" << endl;
+		cin >> caught;
+		fish[i] = caught;
+	}
+	// write who caught what
+	for(int i = 0; i < sizeof(fish)/sizeof(fish[0]); i++){
+		cout << "Fisherman " << i+1	<< " caught " << fish[i] << " fish!" << endl;
+	}
+	// Print the average and biggest catches of the day!
+	cout << "The average number of fish is: " << average(fish) << endl;
+	cout << "The biggest catch of the day was " << highest(fish) << " fish!" << endl;
+	system("pause");
+	return 0;
+}
+
+// Definition
+
+int average(int arr[]){
+	double total = 0;
+	double avg = 0;
+	const int size = 10;
+	for(int i = 0; i < 10; i++){
+		total += arr[i];
+	}
+	avg = total/size;
+	return avg;
+}
+
+int highest(int arr[]){
+	int highest = 0;
+	for(int i = 0; i < 10; i++){
+		if(arr[i] > highest){
+			highest = arr[i];
+		}
+	}
+	return highest;
+}
